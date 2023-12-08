a1ac0b2e523e2a64b19a3715acf4d8c4c77ed63e
Vishnukanthan RASENTHIRAM <vrasenthiram@a203-14.iut.univ-paris8.fr>
2023-12-08 16:23:17 +0100

    ajout couleur

--- Fonction.java
+++ Fonction.java
@@ -3,9 +3,8 @@ public class Fonction {
     public static void main(String[] args) {
         String[][] plateau=creePlateau(10, 10);
         plateau=initPions(plateau);
-        deplacerPion(plateau,6,0,5,1);
-        deplacerPion(plateau, 5, 1, 4, 2);
-        deplacerPion(plateau, 4, 2, 3, 1);
+        
+        
         afficheTableau(plateau);
     }
 
@@ -35,30 +34,48 @@ public class Fonction {
     }
 
     public static void afficheTableau(String[][] t){
-        System.out.println("_Y 0 1 2 3 4 5 6 7 8 9");
-        System.out.println("X|---------------------");
+        String ANSI_RESET = "\u001B[0m"; /* Couleur reset */
+        String ANSI_RED="\u001B[31m"; /* Couleur rouge */
+        String ANSI_BLUE="\u001B[34m";/* Couleur Bleu */
+        
+        
+        System.out.println("_"+ANSI_RED+"Y"+ANSI_RESET+" 0 1 2 3 4 5 6 7 8 9");
+        System.out.println(ANSI_BLUE+"X"+ANSI_RESET+"|---------------------");
+        
         for (int ligne = 0; ligne < t.length; ligne++) {
+
             System.out.print(ligne+"|");
+
             for (int col = 0; col < t.length; col++) {
-               
+
                 System.out.print(t[ligne][col]);
+
             }
+
             System.out.println();
         }
+        
     }
 
 
     public static String[][] initPions(String[][] tab){
+        String ANSI_RESET = "\u001B[0m"; /* Couleur reset */
+        String ANSI_RED="\u001B[31m"; /* Couleur rouge */
+        String ANSI_BLUE="\u001B[34m";/* Couleur Bleu */
         for (int ligne = 0; ligne < tab.length; ligne++) {
             
             for (int col = 0; col < tab[ligne].length; col++) {
                 
                 
                     if((ligne+col)%2==0 && ligne <4){
-                        tab[ligne][col]=" ⛂"; /*⛂⛀ */
+    
+                        tab[ligne][col]=ANSI_RED+" ⛂"+ANSI_RESET; /*⛂⛀ */
+                    
                     }
                     else if((ligne+col)%2==0 && ligne >5){
-                        tab[ligne][col]=" ⛀";
+                        
+                        tab[ligne][col]=ANSI_BLUE+" ⛀"+ANSI_RESET;
+                    
                     }
                 
                 
@@ -80,34 +97,24 @@ public class Fonction {
  * 4: Effectue le déplacement.
  */
 
- public static boolean deplacerPion(String[][] plateau, int xDepart, int yDepart, int xArrivee, int yArrivee) {
-    boolean aEteDeplace=false;
-    if (coordValides(xDepart, yDepart, plateau) && coordValides(xArrivee, yArrivee, plateau)) {
-        if (Math.abs(xArrivee - xDepart) == 1 && Math.abs(yArrivee - yDepart) == 1) {
-            if (plateau[xArrivee][yArrivee].equals(" ■")||plateau[xArrivee][yArrivee].equals(" □")) {
-                String a=plateau[xArrivee][yArrivee];
-                plateau[xArrivee][yArrivee] = " ⛀";
-                plateau[xDepart][yDepart] = a;
-                aEteDeplace=true;
-            } 
-            else{
-                System.out.println("La case n'est pas vide.");
-            }
-        } 
-        else{
-            System.out.println("Le déplacement doit être en diagonale et d'une case.");
-        }
-    } 
-    else {
-        System.out.println("Coordonnées invalides.");
-    }
+ public static void deplacerPion(String[][] plateau, int xDepart, int yDepart, int xArrivee, int yArrivee) {
+   
+    String transi=plateau[xArrivee][yArrivee];
+    plateau[xArrivee][yArrivee] = plateau[xDepart][yDepart];
+    plateau[xDepart][yDepart] = transi;
 
-    return aEteDeplace;
 }
+
+
+
 
 public static boolean coordValides(int x, int y, String[][] plateau) {
     return x >= 0 && x < plateau.length && y >= 0 && y < plateau[0].length;
 }
+
+/*public static int[] deplacementPossible(String[][] plateau,int xPion, int yPion){
+
+}*/
 
 
 
