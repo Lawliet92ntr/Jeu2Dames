111a6cca5762e0500378045713cf9fa7bc2acc1a
Vishnukanthan RASENTHIRAM <vrasenthiram@a203-14.iut.univ-paris8.fr>
2023-12-08 15:41:06 +0100

    changement affichage et initialisation du plateau

--- Fonction.java
+++ Fonction.java
@@ -3,28 +3,29 @@ public class Fonction {
     public static void main(String[] args) {
         String[][] plateau=creePlateau(10, 10);
         plateau=initPions(plateau);
+        deplacerPion(plateau,6,0,5,1);
+        deplacerPion(plateau, 5, 1, 4, 2);
+        deplacerPion(plateau, 4, 2, 3, 1);
         afficheTableau(plateau);
     }
 
     public static String[][] creePlateau(int x, int y){
-        String[][] tab=new String[x+1][y+1];
+        String[][] tab=new String[x][y];
 
-        tab[0][0]="x/y";
-        for (int ligne = 1; ligne < tab.length; ligne++) {
+        
+        for (int ligne = 0; ligne < tab.length; ligne++) {
             
-            tab[0][ligne]=" "+(ligne-1);
-            for (int col = 1; col < tab[ligne].length; col++) {
-                if((ligne-1)==0){
-                    tab[col][0]=" "+(col-1)+"|";
-                }
-                if(ligne!=0 ){
+            
+            for (int col = 0; col < tab[ligne].length; col++) {
+                
+                
                     if((ligne+col)%2==0){
                         tab[ligne][col]=" ■"; /*⛂⛀ */
                     }
                     else{
                         tab[ligne][col]=" □";
                     }
-                }
+                
                 
                 
             }
@@ -34,9 +35,13 @@ public class Fonction {
     }
 
     public static void afficheTableau(String[][] t){
-        for (int i = 0; i < t.length; i++) {
-            for (int j = 0; j < t.length; j++) {
-                System.out.print(t[i][j]);
+        System.out.println("_Y 0 1 2 3 4 5 6 7 8 9");
+        System.out.println("X|---------------------");
+        for (int ligne = 0; ligne < t.length; ligne++) {
+            System.out.print(ligne+"|");
+            for (int col = 0; col < t.length; col++) {
+               
+                System.out.print(t[ligne][col]);
             }
             System.out.println();
         }
@@ -44,18 +49,18 @@ public class Fonction {
 
 
     public static String[][] initPions(String[][] tab){
-        for (int ligne = 1; ligne < tab.length; ligne++) {
+        for (int ligne = 0; ligne < tab.length; ligne++) {
             
-            for (int col = 1; col < tab[ligne].length; col++) {
+            for (int col = 0; col < tab[ligne].length; col++) {
                 
-                if(ligne!=0 ){
-                    if((ligne+col)%2==0 && ligne <5){
+                
+                    if((ligne+col)%2==0 && ligne <4){
                         tab[ligne][col]=" ⛂"; /*⛂⛀ */
                     }
-                    else if((ligne+col)%2==0 && ligne >6){
+                    else if((ligne+col)%2==0 && ligne >5){
                         tab[ligne][col]=" ⛀";
                     }
-                }
+                
                 
                 
             }
@@ -64,7 +69,7 @@ public class Fonction {
 
         return tab;
     }
-}
+
 
 
 /*
@@ -75,12 +80,15 @@ public class Fonction {
  * 4: Effectue le déplacement.
  */
 
- public static void deplacerPion(String[][] plateau, int xDepart, int yDepart, int xArrivee, int yArrivee) {
+ public static boolean deplacerPion(String[][] plateau, int xDepart, int yDepart, int xArrivee, int yArrivee) {
+    boolean aEteDeplace=false;
     if (coordValides(xDepart, yDepart, plateau) && coordValides(xArrivee, yArrivee, plateau)) {
         if (Math.abs(xArrivee - xDepart) == 1 && Math.abs(yArrivee - yDepart) == 1) {
-            if (plateau[xArrivee][yArrivee].equals("0")) {
-                plateau[xArrivee][yArrivee] = plateau[xDepart][yDepart];
-                plateau[xDepart][yDepart] = "0";
+            if (plateau[xArrivee][yArrivee].equals(" ■")||plateau[xArrivee][yArrivee].equals(" □")) {
+                String a=plateau[xArrivee][yArrivee];
+                plateau[xArrivee][yArrivee] = " ⛀";
+                plateau[xDepart][yDepart] = a;
+                aEteDeplace=true;
             } 
             else{
                 System.out.println("La case n'est pas vide.");
@@ -93,19 +101,14 @@ public class Fonction {
     else {
         System.out.println("Coordonnées invalides.");
     }
+
+    return aEteDeplace;
 }
 
 public static boolean coordValides(int x, int y, String[][] plateau) {
     return x >= 0 && x < plateau.length && y >= 0 && y < plateau[0].length;
 }
 
-public static void afficherPlateau(String[][] plateau) {
-    for (String[] ligne : plateau) {
-        for (String casePlateau : ligne) {
-            System.out.print(casePlateau + " ");
-        }
-        System.out.println();
-    }
-    System.out.println();
-}
 
+
+}
